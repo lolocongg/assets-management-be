8568af8770fa943820856ac302d15a8619333040
lolocongg <111275504+lolocongg@users.noreply.github.com>
2026-03-02 17:36:28 +0700

    Update database.go

--- internal/database/database.go
+++ internal/database/database.go
@@ -3,20 +3,25 @@ package database
 import (
 	"context"
 	"fmt"
+	"os"
 
-	"github.com/davidcm146/assets-management-be.git/internal/config"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func NewDB(ctx context.Context, dbCfg config.DBConfig) (*pgxpool.Pool, error) {
-	connectionString := fmt.Sprintf(
-		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
-		dbCfg.User, dbCfg.Password, dbCfg.Host, dbCfg.Port, dbCfg.DBName,
-	)
+func NewDB(ctx context.Context) (*pgxpool.Pool, error) {
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		return nil, fmt.Errorf("DATABASE_URL is not set")
+	}
 
-	pool, err := pgxpool.New(ctx, connectionString)
+	pool, err := pgxpool.New(ctx, dsn)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create db pool: %w", err)
+	}
+
+	// Ping thử DB để chắc chắn connect được
+	if err := pool.Ping(ctx); err != nil {
+		return nil, fmt.Errorf("failed to ping db: %w", err)
 	}
 
 	return pool, nil
